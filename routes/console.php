c4b71df801206424d88217e6975fcc25afec0e63
Produde (Long) <bigtimegangstar@gmail.com>
2024-09-19 10:10:48 +0700

    update: command to create default user

--- routes/console.php
+++ routes/console.php
@@ -14,3 +14,17 @@ Artisan::command('db:rebuild', function () {
     Artisan::call('db:seed');
     $this->comment('Database has been rebuilt successfully.');
 })->purpose('Wipe, migrate, and seed the database');
+
+Artisan::command('make:user-default', function () {
+    $defaultName = env('DEFAULT_NAME');
+    $defaultEmail = env('DEFAULT_EMAIL');
+    $defaultPassword = env('DEFAULT_PASSWORD');
+
+    Artisan::call('make:filament-user', [
+        '--name' => $defaultName,
+        '--email' => $defaultEmail,
+        '--password' => $defaultPassword,
+    ]);
+
+    $this->comment('Default user created successfully.');
+})->purpose('Create a default user from environment variables');
